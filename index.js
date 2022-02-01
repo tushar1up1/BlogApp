86c9728039bdaca51a5e9f08cc8fc55d3cdc31fb
Tushar Upadhyay <tusharup@gmail.com>
2022-01-31 21:57:43 -0500

    Added EJS templating engine

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs')
+
 const app = new express()
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(4000, ()=>{
-    console.log('App listening on port 4000')
+app.set('view engine', 'ejs');
+
+app.listen(3000, ()=>{
+    console.log('App listening on port 3000')
 })
 
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+app.get("/", function(req, res){
+    //res.sendFile(path.resolve(__dirname, 'pages/index.html'));  
+    res.render('index');
 })
 
-app.get('/about', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+app.get("/about", function(req, res){
+    //res.sendFile(path.resolve(__dirname, 'pages/about.html')); 
+    res.render('about');   
 })
 
-app.get('/post', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+app.get("/post", function(req, res){
+    res.render('post');
+    //res.sendFile(path.resolve(__dirname, 'pages/post.html'));    
+})
+app.get("/contact", function(req, res){
+    res.render('contact');
+    //res.sendFile(path.resolve(__dirname, 'pages/contact.html'));    
 })
 
-app.get('/contact', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
-})
